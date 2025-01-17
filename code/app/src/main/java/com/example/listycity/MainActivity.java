4b5ffee943c775633d7e0b02748ff058429bde97
daniil-zahoruiko <daniel.zagoruyko@gmail.com>
2025-01-16 19:14:46 -0700

    Complete demo and participation exercise

---
+++ code/app/src/main/java/com/example/listycity/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.listycity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView cityList;
+    Button addCityButton;
+    EditText cityInput;
+    ArrayAdapter<String> cityAdapter;
+    ArrayList<String> dataList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EdgeToEdge.enable(this);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        cityList = findViewById(R.id.city_list);
+        addCityButton = findViewById(R.id.add_button);
+        cityInput = findViewById(R.id.city_input);
+
+        String[] cities = {"Edmonton", "Paris", "London", "Ottawa", "Berlin"};
+
+        dataList = new ArrayList<>();
+        dataList.addAll(Arrays.asList(cities));
+
+        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        cityList.setAdapter(cityAdapter);
+
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                cityAdapter.remove(cityAdapter.getItem(position));
+            }
+        });
+
+        addCityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Editable input = cityInput.getText();
+                if(input != null) {
+                    String inputString = input.toString();
+                    if(!inputString.isEmpty()) {
+                        cityAdapter.add(input.toString());
+                    }
+                }
+            }
+        });
+    }
+}
